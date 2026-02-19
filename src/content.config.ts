cb3757c78e963b02fdfcfa7320fb53e8e5825ba3
DComTech-ship-it <dxmondtecku@gmail.com>
2026-02-19 17:39:07 +0000

    Add back BLOG_PATH export for getPath.ts dependency

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,6 +2,8 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 import { SITE } from "@/config";
 
+export const BLOG_PATH = "src/data/blog";
+
 const blog = defineCollection({
   loader: glob({ pattern: "**/[^_]*.md", base: "./src/data/blog" }),
   schema: z.object({
